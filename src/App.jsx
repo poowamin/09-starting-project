a8bdae041500759b20e2ab312f2d3ba0c1a8ff64
poowamin <james.poowamin7@gmail.com>
2026-01-23 23:27:53 +0700

    ProjectSidebar&Styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import ProjectSidebar from "./component/ProjectSidebar";
 
 function App() {
   return (
-    <>
+    <main className ="h-screen my-8">
       <ProjectSidebar />
-    </>
+    </main>
   );
 }
 
