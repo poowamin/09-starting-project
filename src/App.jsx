32122d3436825814f1820a883a897cdf18926de6
poowamin <james.poowamin7@gmail.com>
2026-01-26 16:14:16 +0700

    add new function

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
+import { useState } from "react";
+
 import ProjectSidebar from "./components/ProjectSidebar";
-// import NewProject from "./components/NewProject";
+import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProject: undefined,
+    project: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProject: null,
+      };
+    });
+  }
+
+  let content;
+
+  if (projectState.selectedProject === null) {
+    content = <NewProject />;
+  } else if (projectState.selectedProject === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
-    <main className ="h-screen my-8 flex gap-8">
-      <ProjectSidebar />
-      <NoProjectSelected/>
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
